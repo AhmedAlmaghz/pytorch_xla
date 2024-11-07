0db8c9cfcf9b08ed7f005ee7b267aee79a9f85cf
Ahmed Almaghz <53489256+AhmedAlmaghz@users.noreply.github.com>
2024-11-08 00:02:17 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ release = 'master'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'ar'
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
